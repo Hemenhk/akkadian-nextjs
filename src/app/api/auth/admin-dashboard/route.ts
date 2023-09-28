b697991a87c8fe0cbda449196c5829807edd73aa
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-09-28 08:52:49 +0200

    create mongoose schema for admin dashboard

--- src/app/api/auth/admin-dashboard/route.ts
+++ src/app/api/auth/admin-dashboard/route.ts
@@ -1,5 +1,32 @@
-import { NextRequest } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import AdminDashboard from "@/models/adminDashboard";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    
+  await connectToDatabase();
+  try {
+    const reqBody = await req.json()
+
+    const adminDashboard = await AdminDashboard.create(reqBody);
+
+    if (!req.body) {
+      throw new Error("Nothing was passed into the form!");
+    }
+
+    return NextResponse.json({
+      status: 201,
+      success: true,
+      data: {
+        dashboard: adminDashboard,
+      },
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      status: 400,
+      body: {
+        success: false,
+        error: error.message,
+      },
+    });
+  }
 }
---
+++ src/models/adminDashboard.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export interface AdminDashboardDocument extends Document {
+  announcementText: string;
+  announcementColor: string;
+  heroImage: string;
+  heroHeading: string;
+  heroSubHeading: string;
+  heroButtonText: string;
+  heroButtonColor: string;
+}
+
+const adminDashboard = new mongoose.Schema({
+  announcementText: {
+    type: String,
+    required: [true, "A store must have an announcement text"],
+  },
+  announcementColor: {
+    type: String,
+    required: [true, "A store must have an announcement color"],
+  },
+  heroImage: {
+    type: String,
+    required: [true, "A store must have a hero image"],
+  },
+  heroHeading: {
+    type: String,
+    required: [true, "A store must have a hero heading"],
+  },
+  heroSubHeading: {
+    type: String,
+    required: [true, "A store must have a hero sub-heading"],
+  },
+  heroButtonText: {
+    type: String,
+  },
+  heroButtonColor: {
+    type: String,
+  },
+});
+
+const AdminDashboard: Model<AdminDashboardDocument> =
+  mongoose.models.AdminDashboard ||
+  mongoose.model<AdminDashboardDocument>("AdminDashboard", adminDashboard);
+
+export default AdminDashboard;
