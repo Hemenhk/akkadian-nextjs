ae2a8c687265179c4ef633c8b8b88ed5ed307434
Hemenhk <hemen.hiwakamal@hotmail.com>
2024-03-05 17:52:58 +0100

    add loading state to when app first loads

--- src/components/homepage/TheLoadingScreen.tsx
+++ src/components/homepage/TheLoadingScreen.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import React from "react";
-import logo from "../../assets/images/akkadian-logo.png";
 import Image from "next/image";
 
 export default function TheLoadingScreen() {
   return (
     <div className="absolute top-0 left-0 h-screen w-screen flex justify-center items-center bg-[#fafafa]">
-      <Image src={logo} alt="bg-logo" height={400} width={400} className="animate-logo-scale" />
+      <Image src="/akkadian-logo.png" alt="bg-logo" height={400} width={400} className="animate-logo-scale" />
     </div>
   );
 }
